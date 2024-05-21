af087fd3dd17ae1c24b7cbaaa8bf5eab672925f2
Junaid <130550913+jdKheppar@users.noreply.github.com>
2024-05-21 18:15:52 +0500

    Db Conn and MVC in action

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using Jokes.Data;
+using Jokes.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Jokes.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            //List<Category> objCategoryList = _db.Categories.ToList();
+            return View();
+        }
+    }
+}
---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Jokes.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Jokes.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        //The below constructor will pass the options got from the Progrma.cs to the DBContext
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+            
+        }
+        //For creating a table named Categories on the basis of Model Category using EF Core
+        public DbSet<Category> Categories { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "History", DisplayOrder = 3 }
+                );
+        }
+    }
+}
