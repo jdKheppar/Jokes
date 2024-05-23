230cd2db0cf62b36cb95c36fb9d23524e844e75c
Junaid <130550913+jdKheppar@users.noreply.github.com>
2024-05-23 11:57:18 +0500

    CRUD in process

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -16,7 +16,92 @@ namespace Jokes.Controllers
         public IActionResult Index()
         {
             //List<Category> objCategoryList = _db.Categories.ToList();
+            //return View(objCategoryList);
             return View();
+        }
+        public IActionResult Create()//new Category
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if(obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+            }
+            if(ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category created successfully";//For storing data temporarily, used for sending data from one component to the other
+                return RedirectToAction("Index");
+            }
+            return View();
+           
+            
+            //If we are in the same controller we don't need to give the controller name
+        }
+
+
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb =_db.Categories.Find(id);//Only works on the primary key
+            //Category? categroyFromDb1 = _db.Categories.FirstOrDefault(u => u.Id = id);//Works on all the fields
+            //Category? categoryFromDb2 = _db.Categories.Where(u=>u.Id == id).FirstOrDefault();
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+           
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+
+        }
+
+        //Delete request logic is below
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find(id);//Only works on the primary key
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Category obj = _db.Categories.Find(id); 
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+
         }
     }
 }
