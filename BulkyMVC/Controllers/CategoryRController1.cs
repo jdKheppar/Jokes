5b9f710f759580209748722754565a57f3e87bfb
Muhammad Junaid <mjunaid@LMC-3595>
2024-06-03 19:09:16 +0500

    Repository Pattern Crud Intro

---
+++ BulkyMVC/Controllers/CategoryRController1.cs
@@ -0,0 +1,33 @@
+﻿using Jokes.Data;
+using Jokes.Models;
+using Jokes.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Jokes.Controllers
+{
+    public class CategoryRController1 : Controller
+    {
+        private readonly ICategoryRepository _categoryRepo;
+        public CategoryController(ICategoryRepository categoryRepo)
+        {
+            _categoryRepo = categoryRepo;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _categoryRepo.GetAll().ToList();
+            return View(objCategoryList);
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if(ModelState.IsValid)
+            {
+                _categoryRepo.Add(obj);
+                _categoryRepo.Save();
+            }
+            return View();
+        }
+    }
+}
