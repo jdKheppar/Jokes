5cad806201f1cc22210d754d46fa617a8f8d4fa4
Junaid <130550913+jdKheppar@users.noreply.github.com>
2024-06-05 18:15:13 +0500

    Unit of Work

---
+++ BulkyMVC/Repository/IRepository/UnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using Jokes.Data;
+using Jokes.Models;
+
+namespace Jokes.Repository.IRepository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private ApplicationDbContext _db;
+        public ICategoryRepository CategoryRepository { get; private set; }
+        public UnitOfWork(ApplicationDbContext db)
+        {
+            _db = db;
+            Category = new CategoryRepository(_db);
+        }
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
